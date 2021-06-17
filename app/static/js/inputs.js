5425b4b6361b974ac1c4bb8bfe53685f288bdd93
Ian Chen-Adamczyk <48662100+ichenadamczyk20@users.noreply.github.com>
2021-06-16 22:45:05 -0400

    Update inputs.js

--- app/static/js/inputs.js
+++ app/static/js/inputs.js
@@ -11,15 +11,12 @@
     function keydown(e) {
         switch(e.keyCode) {
             case 37: //left
-                e.preventDefault();
                 inputs.left = true;
                 break;
             case 65: //a
-                e.preventDefault();
                 inputs.left = true;
                 break;
             case 74: //j
-                e.preventDefault();
                 inputs.left = true;
                 break;
             case 38: //up
@@ -27,27 +24,21 @@
                 inputs.up = true;
                 break;
             case 87: //w
-                e.preventDefault();
                 inputs.up = true;
                 break;
             case 73: //i
-                e.preventDefault();
                 inputs.up = true;
                 break;
             case 32: //spacebar
-                e.preventDefault();
                 inputs.up = true;
                 break;
             case 39: //right
-                e.preventDefault();
                 inputs.right = true;
                 break;
             case 76: //l
-                e.preventDefault();
                 inputs.right = true;
                 break;
             case 68: //d
-                e.preventDefault();
                 inputs.right = true;
                 break;
         }
@@ -56,15 +47,12 @@
     function keyup(e) {
         switch(e.keyCode) {
             case 37: //left
-                e.preventDefault();
                 inputs.left = false;
                 break;
             case 65: //a
-                e.preventDefault();
                 inputs.left = false;
                 break;
             case 74: //j
-                e.preventDefault();
                 inputs.left = false;
                 break;
             case 38: //up
@@ -72,33 +60,27 @@
                 inputs.up = false;
                 break;
             case 87: //w
-                e.preventDefault();
                 inputs.up = false;
                 break;
             case 73: //i
-                e.preventDefault();
                 inputs.up = false;
                 break;
             case 32: //spacebar
-                e.preventDefault();
                 inputs.up = false;
                 break;
             case 39: //right
-                e.preventDefault();
                 inputs.right = false;
                 break;
             case 76: //l
-                e.preventDefault();
                 inputs.right = false;
                 break;
             case 68: //d
-                e.preventDefault();
                 inputs.right = false;
                 break;
         }
     };
 
     //adding the event listeners
-    document.querySelector("canvas").addEventListener("keydown", keydown);
-    document.querySelector("canvas").addEventListener("keyup", keyup);
+    document.body.addEventListener("keydown", keydown);
+    document.body.addEventListener("keyup", keyup);
 }
